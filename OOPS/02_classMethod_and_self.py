992b2354c3f9a230d06ec7fc955cd4370d371494
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-12-02 18:18:00 +0530

    class method and self

--- OOPS/02_classMethod_and_self.py
+++ OOPS/02_classMethod_and_self.py
@@ -1,12 +1,16 @@
-# Basic class and object
+# Class Method and Self
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
+    
+    def full_name(self):
+        return f"{self.brand} {self.model}"
 
 my_car = Car("Mahindra", "Thar")
 print(my_car.brand)
 print(my_car.model)
+print(my_car.full_name())
 
 my_new_car = Car("Tata", "Range Rover")
 print(my_new_car.brand)
